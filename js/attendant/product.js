597d24a55e38c36b233ec27d17c66b35d26d8693
Qadriyah <b.alzawad@gmail.com>
2018-11-25 08:15:22 +0300

    Feat: The stores attendant should be able to get a specific product
    This commit delivers a fetch API that enables a sales attendant to
    view a specific product.
    [Delivers #162029392]

--- js/attendant/product.js
+++ js/attendant/product.js
@@ -3,6 +3,7 @@ const spinner = '<img src="../media/loader.gif" style="width: 100px" />';
 const errors = document.getElementById('errors');
 const productList = document.getElementById('product');
 const productView = document.getElementById('pview');
+const pform = document.getElementById('pform');
 
 // Get product list
 errors.innerHTML = spinner;
@@ -34,4 +35,31 @@ api.get('/products').then(data => {
   }
 });
 
-// Get
+// Get a specific product
+if (pform) {
+  pform.addEventListener('submit', event => {
+    event.preventDefault();
+    const product_id = Number(document.getElementById('product').value);
+    errors.innerHTML = spinner;
+    api.get('/products/' + product_id).then(data => {
+      const { products, msg } = data;
+      if (msg === 'Success') {
+        let view = [];
+        let counter = 1;
+        products.forEach(product => {
+          view += `
+            <tr>
+                <td>${counter}</td>
+                <td>${product.product_name}</td>
+                <td>${product.category_name}</td>
+                <td class="align-right">${product.product_price.toLocaleString()}</td>
+            </tr>
+            `;
+          counter++;
+        });
+        errors.innerHTML = null;
+        productView.innerHTML = view;
+      }
+    });
+  });
+}
